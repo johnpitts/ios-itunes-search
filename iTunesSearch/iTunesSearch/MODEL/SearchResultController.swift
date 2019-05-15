54d8aa4070d434ebca02a6031cc414f6db5c8fa8
John Pitts <jgpitts@gmail.com>
2019-05-14 20:23:44 -0400

    Created storyboard layout set up tableview for search bar added base url set up properties for search results controller

---
+++ iTunesSearch/iTunesSearch/MODEL/SearchResultController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchResultController.swift
+//  iTunesSearch
+//
+//  Created by John Pitts on 5/14/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import Foundation
+
+class SearchResultController {
+    
+    let baseURL = URL(string: "https://itunes.apple.com")!
+    
+    var searchResults: [SearchResult] = []
+}
